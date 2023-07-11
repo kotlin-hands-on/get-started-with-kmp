412da8fa1ff6043c14c2e6116bf0d081fb892d8e
pahill <pamela.a.hill@gmail.com>
2023-07-11 14:28:00 +0200

    Added step 3 of the tutorial

---
+++ step3/final/shared/src/commonMain/kotlin/com/jetbrains/simplelogin/kotlinmultiplatformsandbox/Greeting.kt
@@ -0,0 +1,10 @@
+package com.jetbrains.simplelogin.kotlinmultiplatformsandbox
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Guess what it is! > ${platform.name.reversed()}!" +
+                "\nThere are only ${daysUntilNewYear()} days left until New Year! 🎆"
+    }
+}
