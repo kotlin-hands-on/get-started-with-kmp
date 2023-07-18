4fa2683979e5965d3f999cb852e5e74589b962e0
pahill <pamela.a.hill@gmail.com>
2023-07-18 15:20:18 +0200

    Downgrade KMP-NativeCoroutines as we're not ready to go up to 1.9.0 yet

--- step4/build.gradle.kts
+++ step4/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("multiplatform").version("1.8.22").apply(false)
     kotlin("plugin.serialization").version("1.8.22").apply(false)
     id("com.google.devtools.ksp").version("1.8.22-1.0.11").apply(false)
-    id("com.rickclephas.kmp.nativecoroutines").version("1.0.0-ALPHA-13").apply(false)
+    id("com.rickclephas.kmp.nativecoroutines").version("1.0.0-ALPHA-12").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
