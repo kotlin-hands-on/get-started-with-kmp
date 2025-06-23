a47107a5362b67d0786faa9261e8523a10afd181
Ilya Evtushenko <evt.ilyaa@gmail.com>
2025-06-23 13:20:26 +0200

    Add a final step example

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import Shared
+import KMPNativeCoroutinesAsync
+import KMPNativeCoroutinesCore
+
+struct ContentView: View {
+    @ObservedObject private(set) var viewModel: ViewModel
+
+    var body: some View {
+        ListView(phrases: viewModel.greetings)
+            .task { await self.viewModel.startObserving() }
+    }
+}
+
+extension ContentView {
+    @MainActor
+    class ViewModel: ObservableObject {
+        @Published var greetings: Array<String> = []
+
+        func startObserving() async {
+            do {
+                let sequence = asyncSequence(for: Greeting().greet())
+                for try await phrase in sequence {
+                    self.greetings.append(phrase)
+                }
+            } catch {
+                print("Failed with error: \(error)")
+            }
+        }
+    }
+}
+
+struct ListView: View {
+    let phrases: Array<String>
+
+    var body: some View {
+        List(phrases, id: \.self) {
+            Text($0)
+        }
+    }
+}
---
+++ shared/build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization") version "2.1.21"
+    id("com.google.devtools.ksp")
+    id("com.rickclephas.kmp.nativecoroutines")
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
+
+    val ktorVersion = "3.1.3"
+
+    sourceSets {
+        all {
+            languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+        }
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+            implementation("io.ktor:ktor-client-core:${ktorVersion}")
+            implementation("io.ktor:ktor-client-content-negotiation:${ktorVersion}")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
+        }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "com.jetbrains.greeting.greetingkmp.shared"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
