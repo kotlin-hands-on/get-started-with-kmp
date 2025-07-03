8c74f98b24ecc292a7a9e023a456c35d811ff992
Ilya Evtushenko <evt.ilyaa@gmail.com>
2025-07-03 20:37:23 +0200

    Bump kotlinx datetime version

--- shared/src/commonMain/kotlin/com/jetbrains/greeting/greetingkmp/RocketComponent.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/greeting/greetingkmp/RocketComponent.kt
@@ -5,10 +5,10 @@ import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.*
-import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.json.Json
+import kotlin.time.Instant
 
 class RocketComponent {
     private val httpClient = HttpClient {
@@ -28,7 +28,7 @@ class RocketComponent {
         val date = Instant.parse(lastSuccessLaunch.launchDateUTC)
             .toLocalDateTime(TimeZone.currentSystemDefault())
 
-        return "${date.month} ${date.dayOfMonth}, ${date.year}"
+        return "${date.month} ${date.day}, ${date.year}"
     }
 
     suspend fun launchPhrase(): String =
