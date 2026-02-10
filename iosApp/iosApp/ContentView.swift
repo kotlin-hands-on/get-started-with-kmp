065ecea5803a1fc14351e6278cc449fa4ba577ab
Ilya Evtushenko <evt.ilyaa@gmail.com>
2026-02-10 13:16:21 +0100

    Migrate to AGP 9 and new project structure, and upgrade dependencies

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-import Shared
+import SharedLogic
 import KMPNativeCoroutinesAsync
 import KMPNativeCoroutinesCore
 
---
+++ sharedLogic/src/commonTest/kotlin/com/jetbrains/greeting/greetingkmp/SharedLogicCommonTest.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.greeting.greetingkmp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedLogicCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
